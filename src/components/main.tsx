beb4eefb3d03488c8c693edfe336fb1cb9226e9c
reemo <r48n34@gmail.com>
2023-02-08 00:17:03 +0800

    add hash and form

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -1,17 +1,33 @@
 import { useState } from "react"
-import { Button, Textarea, PasswordInput, CopyButton, ActionIcon, Tooltip, Grid } from '@mantine/core';
+import { Button, Textarea, PasswordInput, Grid } from '@mantine/core';
+import { useForm } from '@mantine/form';
+
 import { IconCheck, IconCopy } from "@tabler/icons";
 import CopyTextIcon from "./CopyTextIcon";
+import sha256 from 'crypto-js/sha256';
 
 export function Main({ name = "Extension" }) {
   
     const [ domain, setDomain ] = useState<string>("");
     const [ password, setPassword ] = useState<string>("");
 
+    const form = useForm({
+        initialValues: {
+            domain: "https://www.google.com/",
+            password: "",
+        },
+    
+        validate: {
+            domain: (value) => (value.length >= 1) ? null : 'Invalid domain',
+            password: (value) => (value.length >= 1) ? null : 'Invalid password',
+        },
+    });
+
     const [ hashedPassword, setHashedPassword ] = useState<string>("");
 
-    function hashPw(){
-        setHashedPassword(password)
+    function hashPw(values: {domain: string, password: string}){
+        let finalStr = values.domain.trim() + values.password
+        setHashedPassword(sha256(finalStr))
     }
 
     return (
@@ -23,22 +39,24 @@ export function Main({ name = "Extension" }) {
                 width: "300px"
             }}
         >
+            <form onSubmit={form.onSubmit((values) => hashPw(values))}>
             <Textarea 
                 label="Domain"
-                minRows={1}
-                value={domain}
-                onChange={(event) => setDomain(event.currentTarget.value)}
+                withAsterisk
+                minRows={2}
+                {...form.getInputProps('domain')}
             />
 
             <PasswordInput 
-                label="Password" 
+                label="Password"
+                withAsterisk 
                 placeholder="Password" 
                 mt={6}
-                value={password}
-                onChange={(event) => setPassword(event.currentTarget.value)}
+                {...form.getInputProps('password')}
             />
 
-            <Button mt={20} onClick={ () => hashPw() }>Encode</Button>
+            <Button mt={20} type="submit">Encode</Button>
+            </form>
 
             { hashedPassword !== "" && (
                 <>
@@ -59,3 +77,7 @@ export function Main({ name = "Extension" }) {
         </div>
     )
 }
+
+
+// 76d442f15d4e25707c0746162d4c415db2480560bb775308549a6f06f3d7354e
+// 76d442f15d4e25707c0746162d4c415db2480560bb775308549a6f06f3d7354e

c2a8ad26b48d837403f549288cb72a625cb4f6e1
reemo <r48n34@gmail.com>
2023-02-08 14:25:39 +0800

    add self sha

--- src/components/main.tsx
+++ src/components/main.tsx
@@ -3,13 +3,15 @@ import { useForm } from '@mantine/form';
 import { Button, Textarea, PasswordInput, Grid, Select, Badge, Group } from '@mantine/core';
 import CopyTextIcon from "./CopyTextIcon";
 import sha256 from 'crypto-js/sha256';
+// import { sha256 } from "~utilis/sha256";
+
 import toast, { Toaster } from "react-hot-toast";
 import { MantineProvider } from '@mantine/core';
+// import { IconXboxX } from "@tabler/icons";
 
 
 export function Main({ name = "Extension" }) {
 
-    
     try{
         const [ hashedPassword, setHashedPassword ] = useState<string>("");
         const [ showPwLength, setShowPwLength ] = useState<string>("Full");
@@ -28,6 +30,7 @@ export function Main({ name = "Extension" }) {
         function hashPw(values: {domain: string, password: string}){
             let finalStr = values.domain.trim() + values.password
             setHashedPassword(sha256(finalStr));
+            // setHashedPassword(sha256(finalStr));
             toast.success('Successfully to hash!')
         }
 
@@ -35,7 +38,7 @@ export function Main({ name = "Extension" }) {
             <MantineProvider theme={{ colorScheme: 'dark' }} withGlobalStyles withNormalizeCSS>
             <div><Toaster/></div>
     
-            <div style={{ padding: 12, width: "350px" }} >
+            <div style={{ padding: 12, width: "400px" }} >
     
                 <Group position="center" mb={6}>
                     <Badge 
@@ -82,15 +85,13 @@ export function Main({ name = "Extension" }) {
                         label="Length"
                         data={[
                             { value: 'Full', label: 'Full' },
-                            { value: '8', label: '8' },
-                            { value: '16', label: '16' },
-                            { value: '24', label: '24' },
-                            { value: '32', label: '32' },
+                            ...[8,16,24,32,40,48,56].map( v => { return { value: v+"", label: v+"" }} )
                         ]}
                         value={showPwLength}
                         onChange={setShowPwLength}
                     />
-                    <Grid mt={6}>
+
+                    <Grid mt={6} mb={6}>
                         <Grid.Col span={10}>
                             <PasswordInput 
                                 label="Hashed Password" 
@@ -125,3 +126,8 @@ export function Main({ name = "Extension" }) {
     }
 
 }
+
+// 89a1fe75eb7b02031bd6904375b71b4829d6568bfb24dfba982b09ad4e3fe462
+
+// 89a1fe75eb7b02031bd6904375b71b4829d6568bfb24dfba982b09ad4e3fe462
+// 89a1fe75eb7b02031bd6904375b71b4829d6568bfb24dfba982b09ad4e3fe462
